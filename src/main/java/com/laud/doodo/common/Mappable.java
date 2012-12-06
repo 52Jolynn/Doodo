b9701345c5e835793b28cab0b2c7571824bea8d1
52Jolynn <htd0324@gmail.com>
2012-12-06 19:23:34 +0800

    新増类Mappable、ValueVisitor

---
+++ src/main/java/com/laud/doodo/common/Mappable.java
@@ -0,0 +1,37 @@
+package com.laud.doodo.common;
+
+import java.util.Map;
+
+/**
+ * <pre>
+ * T 对象类型
+ * K 键类型
+ * V 值类型
+ * </pre>
+ * 
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2012-11-23 上午9:43:42
+ * @copyright: www.armisi.com.cn
+ */
+public interface Mappable<T, K, V> {
+	/**
+	 * 键值映射集合
+	 * 
+	 * @param visitor
+	 *            值处理器
+	 * @return
+	 */
+	public Map<K, V> map(ValueVisitor<V> visitor);
+
+	/**
+	 * 键值映射转实体对象
+	 * 
+	 * @param map
+	 *            键值集合
+	 * @param visitor
+	 *            值处理器
+	 * @return
+	 */
+	public T set(Map<K, V> map, ValueVisitor<V> visitor);
+}
---
+++ src/main/java/com/laud/doodo/common/ValueVisitor.java
@@ -0,0 +1,31 @@
+package com.laud.doodo.common;
+
+/**
+ * 数值访问器
+ * 
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2012-11-23 上午10:11:25
+ * @copyright: www.armisi.com.cn
+ */
+public interface ValueVisitor<T> {
+	/**
+	 * 将指定值转成指定类型的值
+	 * 
+	 * @param value
+	 *            值
+	 * @return 返回值
+	 */
+	public T accept(Object value);
+
+	/**
+	 * 将值转成目标对象类型的值
+	 * 
+	 * @param targetClass
+	 *            目标对象类型
+	 * @param value
+	 *            值
+	 * @return
+	 */
+	public Object target(Class<?> targetClass, Object value);
+}
