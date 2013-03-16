dcf6f35120310af7776dd120d2bf3c32eeb8c09f
Laud <htd0324@gmail.com>
2013-03-16 16:45:03 +0800

    修复电子邮件正则表达式

--- src/main/java/com/laud/doodo/string/StringUtils.java
+++ src/main/java/com/laud/doodo/string/StringUtils.java
@@ -57,7 +57,7 @@ public class StringUtils {
 	 * @return
 	 */
 	public static boolean checkEmail(String email) {
-		String regex = "([a-zA-Z0-9 \\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})";
+		String regex = "([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})";
 		if (Pattern.matches(regex, email)) {
 			return true;
 		}
