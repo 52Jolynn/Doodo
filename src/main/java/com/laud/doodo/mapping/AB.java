034d3152b0a54b7c6b44f1189e859493de14072b
52Jolynn <htd0324@gmail.com>
2013-03-29 14:59:09 +0800

    加AB 1on1类

---
+++ src/main/java/com/laud/doodo/mapping/AB.java
@@ -0,0 +1,14 @@
+package com.laud.doodo.mapping;
+
+/**
+ * AB 1on1
+ * 
+ * @author: Laud
+ * @email: htd0324@gmail.com
+ * @date: 2013-3-29 下午2:58:09
+ * @copyright: www.armisi.com.cn
+ */
+public class AB<A, B> {
+	public A a;
+	public B b;
+}
